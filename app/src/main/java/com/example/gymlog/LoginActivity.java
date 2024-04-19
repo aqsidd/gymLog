9d6d9053674738fb17b70adee97132477a92bdbe
aqsidd <asiddiqui@csumb.edu>
2024-04-18 18:32:23 -0700

    vid 8 18:00

---
+++ app/src/main/java/com/example/gymlog/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.gymlog;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.gymlog.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+    }
+
+
+    static Intent loginIntentFactory(Context context){
+        return new Intent(context, LoginActivity.class);
+    }
+
+
+}
--- app/src/main/java/com/example/gymlog/MainActivity.java
+++ app/src/main/java/com/example/gymlog/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.gymlog;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
@@ -42,6 +44,15 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        loginUser();
+
+        if(loggedInUserId == -1){
+            Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
+
+
+
         repository = GymLogRepository.getRepository(getApplication());
 
         binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
@@ -58,6 +69,17 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    private void loginUser() {
+
+    }
+
+//    static Intent mainActivityIntentFactory(Context context, int userId){
+//        Intent intent = new Intent(context, MainActivity.class);
+//        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
+//        return intent;
+//    }
+
 
     private void insertGymLogRecord(){
         if(mExercise.isEmpty()){
